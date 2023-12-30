98678bbabf0b622044676ba68091da380ba745de
aldif1 <satrioaldi@outlook.com>
2023-12-30 14:51:48 +0700

    new

--- training.py
+++ training.py
@@ -76,7 +76,7 @@ model = tflearn.DNN(net)
 # try:
 #   model.load("model.tflearn")
 # except:
-model.fit(training, output, n_epoch=1000, batch_size=8, show_metric=True)
+model.fit(training, output, n_epoch=2000, batch_size=10, show_metric=True)
 model.save("modell.h5")
 def bag_of_words(s, words):
   bag = [0 for _ in range(len(words))]
